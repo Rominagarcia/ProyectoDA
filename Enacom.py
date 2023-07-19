6faf644f70f2c88c36c427e8407322e707779c31
Romina Giselle García <96449858+Rominagarcia@users.noreply.github.com>
2023-07-19 11:23:51 -0300

    Add files via upload

---
+++ Enacom.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("Telecomunicaciones")
+
+st.write("Enacom es un organismo autárquico y descentralizado que se encarga de regular las comunicaciones en Argentina. Su misión es garantizar el acceso equitativo de todos los argentinos a servicios como Internet, telefonía, radio, correo postal y televisión. Desde su creación en 2015, Enacom busca promover la inclusión digital, proteger a los usuarios y mejorar la calidad de los servicios de comunicación. Su visión es convertirse en un referente internacional en regulaciones y control de las comunicaciones, generando espacios de inclusión digital y defensa de los usuarios. Para lograrlo, Enacom establece lineamientos que abarcan la universalización de la inclusión digital, la protección de los usuarios, el aumento de la calidad de servicio, la promoción de la competencia, las inversiones en infraestructura digital y la transparencia en la gestión.")
